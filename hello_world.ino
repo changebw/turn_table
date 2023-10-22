b9aa31d39e980d1a84e89e3b8a6dd549915692dc
changebw <103644028+changebw@users.noreply.github.com>
2023-10-22 17:48:15 -0400

    Add files via upload

---
+++ hello_world.ino
@@ -0,0 +1,15 @@
+int i;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.print("Hello World");
+  i = 0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (i < 10) {
+    Serial.print(i);
+  }
+  i += 1;
+}
